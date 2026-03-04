2c67154dfa3a13a76e644bdfd95f5865f28847fc
WilerGamer <fakih455@gmail.com>
2026-03-04 16:46:55 +0200

    fix

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,10 +1,11 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
-  family: 4,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
